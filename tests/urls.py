bb35c370a387eae2b6f24380235ff72117a14c34
Christian <chris@elfsechzehnServer.local>
2022-04-23 11:39:59 +0000

    Init Online

--- tests/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path("", views.index, name="index"), # Das bedeutet bei keinen weiteren Argumenten soll die Index Funktion gerendert werden
-    path("owner_list/", views.OwnerList.as_view() , name="owner_list"),
-    path("owner_create/", views.OwnerCreate.as_view() , name="owner_create"),
-    path("owner_update/<pk>", views.OwnerUpdate.as_view() , name="owner_update"),
-    path("owner_delete/<pk>", views.OwnerDelete.as_view() , name="owner_delete"),
-]
